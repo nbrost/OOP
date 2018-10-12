0ed382e2bc8ffc4e1cbdcd50e0a909f16ae8e07a
Nolan <nbrost@ualberta.ca>
2018-10-12 14:14:13 -0600

    actually finished question 4

--- 4.sql
+++ 4.sql
@@ -1,10 +1,19 @@
 .print Question 1 - nbrost
 .echo on
-select  city
+select requests.rid,requests.email, requests.pickup,requests.dropoff, rides.rno
 from requests
 join locations as a
-on requests.pickup = a.lcode;
-/*join rides as b
-on b.src = locations.lcode
-where a.city = b.city;*/
-
+on requests.pickup = a.lcode
+join locations as c
+on requests.dropoff = c.lcode,
+rides
+join locations as b
+on rides.src = b.lcode
+join locations as d
+on rides.dst =d.lcode
+where a.city = b.city
+and c.city = d.city
+and a.prov = b.prov
+and c.prov = d.prov
+and requests.rdate = rides.rdate
+and rides.price <= requests.amount;
