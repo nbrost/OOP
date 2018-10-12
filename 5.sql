a29cbe5c4a62e9b0946fe84bbbab145b3266ab37
Nolan <nbrost@ualberta.ca>
2018-10-12 14:19:57 -0600

    actually getting a question or two

---
+++ 5.sql
@@ -0,0 +1,8 @@
+.print Question 1 - nbrost
+.echo on
+select locations.city, locations.prov
+from locations
+join rides on rides.dst = locations.lcode
+group by locations.city
+order by count(*) desc
+limit 3;
